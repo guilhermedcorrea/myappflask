d9f8b9ddc3b6798e74f35b4fe9b536b488f71dbe
guilhermedcorrea <78979736+guilhermedcorrea@users.noreply.github.com>
2022-01-27 16:03:50 -0300

    Update config.py

--- config.py
+++ config.py
@@ -1,15 +1,13 @@
 import pyodbc
 from urllib.parse import quote_plus
+import os
+import dotenv
 
-parametros = (
-"Driver={SQL Server Native Client 11.0};"
-"Server=GUILHERME-PC\DEVMSSQLSERVER;"
-"Database=marketplacesellers;"
-"UID=sa;"
-"PWD=Mudar@123"
+dotenv.load_dotenv(dotenv.find_dotenv())
+string_conexao = os.getenv("parametros")
 
-)
 
-url_db = quote_plus(parametros)
+
+url_db = quote_plus(string_conexao)
 SQLALCHEMY_DATABASE_URI = 'mssql+pyodbc:///?odbc_connect=%s' %url_db
 SQLALCHEMY_TRACK_MODIFICATIONS = False
